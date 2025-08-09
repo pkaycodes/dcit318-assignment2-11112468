21014d33ab7a32e4de388ca10f48f2b6d6228485
Papa Kwame Afrifa-Mensa <125192544+pkaycodes@users.noreply.github.com>
2025-08-09 14:22:24 +0200

    Add Interface.cs

---
+++ Interface.cs
@@ -0,0 +1,34 @@
+using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();
+        bicycle.Move();
+    }
+}
