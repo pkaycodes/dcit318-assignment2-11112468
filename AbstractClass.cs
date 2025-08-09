6ec46d84d11d1c60818e490e2bf4b844733d3309
Papa Kwame Afrifa-Mensa <125192544+pkaycodes@users.noreply.github.com>
2025-08-09 14:21:50 +0200

    Add abstract class

---
+++ AbstractClass.cs
@@ -0,0 +1,50 @@
+using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public override double GetArea()
+    {
+        return width * height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine($"Circle area: {circle.GetArea():F2}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea():F2}");
+    }
+}
